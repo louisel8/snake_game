24b465ed875fca89a129c81d237f178cb39ac576
Louise Liu <162784197+louisel8@users.noreply.github.com>
2025-07-06 17:22:29 +0800

    updated with more functions
    detected collisions with food; created a scoreboard and keep score added if had more food; detected collisions with the wall; detected collisions with your own tail. trigger and show game over when game over situations are met.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
+from snake_game.snake_game.food import Food
+from snake_game.snake_game.scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +15,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +27,24 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.2)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
 
 screen.exitonclick()
 
